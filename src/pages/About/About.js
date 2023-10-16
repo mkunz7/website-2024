ef2324b28a5898461a9e0da00dd48addafdf1b46
Jeris Rue <jeris.rue@gmail.com>
2023-10-15 21:43:41 -0500

    temp remove organizers

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -48,7 +48,7 @@ export default class About extends Component {
           <Tab tabFor="kernelcon">Kernelcon</Tab>
           <Tab tabFor="code-of-conduct">Code Of Conduct</Tab>
           <Tab tabFor="reciprocity">Reciprocity</Tab>
-          <Tab tabFor="organizers">Organizers</Tab>
+          {/* <Tab tabFor="organizers">Organizers</Tab> */}
           <Tab tabFor="contact">Contact</Tab>
           <Tab tabFor="faq">FAQ</Tab>
           <Tab tabFor="archive">Archive</Tab>

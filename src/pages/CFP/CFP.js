0d013c97578684e912d3e0927f40258031abe6b2
Tim Vidas <tvidas@users.noreply.github.com>
2023-11-01 23:15:41 -0500

    stray div

--- src/pages/CFP/CFP.js
+++ src/pages/CFP/CFP.js
@@ -16,7 +16,7 @@ export default class CFP extends Component {
 								The 2024 Kernelcon Call for Papers is opening soon.  Please check back later.
 							</p>
 						</div> */}
-						{<div className="text-area">
+						<div className="text-area">
 							
 							{/* Commented out becuase imbedded into OpenCalls */}
 							{/* <h3 className="title">Call for Papers</h3> */}
@@ -26,7 +26,7 @@ export default class CFP extends Component {
 							<p className="tab-paragraph">
 				The Kernelcon Crew is soliciting presentations for the sixth annual Kernelcon held at the Downtown Embassy Suites in Omaha, NE, April 2-5, 2024.
 							</p>
-       						</div>
+       						
 							<p className="tab-paragraph">
 				Create something fun in your homelab? Develop a new technique? Wish you could spare someone the horrors you faced? Impart your wisdom, start the difficult conversations, submit your work to our CFP!
 							</p>
